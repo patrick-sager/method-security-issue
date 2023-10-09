20c6430ba68964d7f03fda6f668700e5b13e634a
Patrick Sager <psager@tridium.com>
2023-10-09 13:01:27 -0400

    Update sample with global method security workaround

--- src/main/java/com/example/methodsecurity/SpringBootMain.java
+++ src/main/java/com/example/methodsecurity/SpringBootMain.java
@@ -2,10 +2,10 @@ package com.example.methodsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-@EnableMethodSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringBootMain {
 
   public static void main(String[] args) {
